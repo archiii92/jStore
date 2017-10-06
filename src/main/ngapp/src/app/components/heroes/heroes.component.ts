b637832eee06467d9fd19401f5de50af14a9d84a
archiii92 <den92n@yandex.ru>
2017-10-06 22:38:02 +0400

    [+] Full-Implemented Angular Tutorial: Tour of Heroes without local server.

---
+++ src/main/ngapp/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { Hero } from '../../entities/hero';
+import { HeroService } from '../../services/hero.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit{
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService  ) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+}
---
+++ src/main/ngapp/src/app/modules/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppComponent } from '../components/app/app.component';
+import { HeroDetailComponent } from '../components/hero-detail/hero-detail.component';
+import { HeroesComponent } from '../components/heroes/heroes.component';
+import { HeroService } from '../services/hero.service';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent] 
+})
+
+export class AppModule { }
