ea9845543caf0a3f3af640d720f8b8a159b46dec
archiii92 <den92n@yandex.ru>
2017-11-02 16:38:40 +0400

    [+] Add external cfg file

--- src/main/java/jStore/configuration/JStoreConfiguration.java
+++ src/main/java/jStore/configuration/JStoreConfiguration.java
@@ -1,13 +1,15 @@
 package jStore.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -22,36 +24,33 @@ import java.util.Map;
 @EnableTransactionManagement
 @EnableWebMvc
 @ComponentScan({"jStore.controllers"})
+@PropertySource("classpath:datasource.properties")
 public class JStoreConfiguration {
+
+    @Autowired
+    Environment env;
 
     @Bean
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql:jStore");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("12345");
+        dataSource.setDriverClassName(env.getRequiredProperty("dataSource.driverClassName"));
+        dataSource.setUrl(env.getRequiredProperty("dataSource.url"));
+        dataSource.setUsername(env.getRequiredProperty("dataSource.username"));
+        dataSource.setPassword(env.getRequiredProperty("dataSource.password"));
         return dataSource;
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DriverManagerDataSource dataSource) {
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setShowSql(Boolean.TRUE);
-        vendorAdapter.setDatabase(Database.POSTGRESQL);
-
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(dataSource);
         entityManagerFactoryBean.setPackagesToScan("jStore.models");
-        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
         Map<String, Object> jpaProperties = new HashMap<>();
         jpaProperties.put("hibernate.hbm2ddl.auto", "none");
         jpaProperties.put("hibernate.show_sql", "true");
         jpaProperties.put("hibernate.format_sql", "true");
-        jpaProperties.put("hibernate.use_sql_comments", "true");
-        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         jpaProperties.put("hibernate.physical_naming_strategy", "jStore.configuration.PhysicalNamingStrategyImpl");
         entityManagerFactoryBean.setJpaPropertyMap(jpaProperties);
 
